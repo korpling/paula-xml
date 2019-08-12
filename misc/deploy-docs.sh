8a8936c2a96091362b6e276c3cc8c1eeda08bfc3
Thomas Krause <thomaskrause@posteo.de>
2019-08-12 09:56:37 +0200

    Report Git errors

--- misc/deploy-docs.sh
+++ misc/deploy-docs.sh
@@ -6,15 +6,15 @@ if [ -n "$GITHUB_API_KEY" ]; then
     cd "$TRAVIS_BUILD_DIR"
 
     echo "cloning gh-pages"
-    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null
+    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null || (echo "Git clone failed. Did you use the correct value for the GITHUB_API_KEY environment variable?" exit 1)
     cd gh-pages
     cp -R ${TRAVIS_BUILD_DIR}/book/* .
     git add .
     git -c user.name='travis' -c user.email='travis' commit -m "update documentation for version ${SHORT_VERSION}"
     echo "pushing to gh-pages"
-    git push -q https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null
+    git push -q https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null || (echo "Git push failed. Did you use the correct value for the GITHUB_API_KEY environment variable?" exit 1)
     cd "$TRAVIS_BUILD_DIR"
 else
-	>&2 echo "Cannot deploy documentation because GITHUB_API_KEY environment variable is not set"
+	echo "Cannot deploy documentation because GITHUB_API_KEY environment variable is not set"
 	exit 1
 fi
