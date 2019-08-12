ef7f836e817464e9224555871bde7e24a5633ff4
Thomas Krause <thomaskrause@posteo.de>
2019-08-12 10:02:41 +0200

    use && to execute exit

--- misc/deploy-docs.sh
+++ misc/deploy-docs.sh
@@ -8,13 +8,13 @@ if [ -n "$GITHUB_API_KEY" ]; then
     rm -Rf gh-pages
 
     echo "cloning gh-pages"
-    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null || (echo "Git clone failed. Did you use the correct value for the GITHUB_API_KEY environment variable?"; exit 1)
+    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null || echo "Git clone failed. Did you use the correct value for the GITHUB_API_KEY environment variable?" && exit 1
     cd gh-pages
     cp -R ${TRAVIS_BUILD_DIR}/book/* .
     git add .
     git -c user.name='travis' -c user.email='travis' commit -m "update documentation for version ${SHORT_VERSION}"
     echo "pushing to gh-pages"
-    git push -q https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null || (echo "Git push failed. Did you use the correct value for the GITHUB_API_KEY environment variable?"; exit 1)
+    git push -q https://$GITHUB_API_KEY@github.com/{TRAVIS_REPO_SLUG} gh-pages &>/dev/null || echo "Git push failed. Did you use the correct value for the GITHUB_API_KEY environment variable?" && exit 1
     cd "$TRAVIS_BUILD_DIR"
 else
 	echo "Cannot deploy documentation because GITHUB_API_KEY environment variable is not set"
